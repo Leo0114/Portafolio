184e1f6e5616f412768615be21b0e0bb8dd7f8c6
Leo0114 <leonel0614@gmail.com>
2023-09-06 23:33:26 -0600

    Gpages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://Leo0114.github.io",
+  base: "/Portafolio",
   integrations: [tailwind()],
-  output: "server",
-  adapter: vercel()
 });
